12a30a1d84215e4e0185e05f8b62811b249fd680
Seth <sethmetcalf@gmail.com>
2014-01-30 19:18:21 -0700

    Sorted

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,19 @@
+app.controller('FriendController', function($scope, $http){
+	$scope.test = 'Hey my angular is working';
+    $http.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json')
+        .success(function(data) {
+            $scope.friends = data.results;
+            angular.forEach($scope.friends, function(friend) {
+                if (friend.friend_count) {
 
 
+                friend.friend_count = parseInt(friend.friend_count, 10);
+            } else {
+                friend.friend_count = 0;
+            }
+            })
+    })
 
-
-
+   $scope.sortField = 'name';
+   $scope.orderField = 'false';
+});
